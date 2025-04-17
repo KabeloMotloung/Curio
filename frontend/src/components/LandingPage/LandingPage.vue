939e8f38ab993b0da1f87990f8e376e47f7dc549
Simphiwe <simphiwe.nonabe@dariel.co.za>
2025-04-17 14:55:22 +0200

    replace HelloWorld component with LandingPage in App.vue and add LandingPage component with image

---
+++ frontend/src/components/LandingPage/LandingPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import imgUrl from '../../assets/LandingPage/ImageOfMuseum.jpeg'
+</script>
+
+<template>
+    <img :src="imgUrl" alt="Image of Museum" class="image-of-museum" />
+</template>
+
+<style>
+.image-of-museum {
+    width: 100%;
+    height: 80vh;
+    /* object-fit: cover; */
+    /* object-position: center; */
+    /* position: absolute; */
+    z-index: -1; /* Ensure the image is behind other content */
+}
+</style>
