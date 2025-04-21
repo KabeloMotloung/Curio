d3b1e78e427fd79b790e43dfe0d88dccb1bdc308
Alec Watson <111232411+Alec-Watson@users.noreply.github.com>
2025-04-21 22:32:54 +0200

    Revert "Landing page"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import TheDiscoveryCard from './components/Cards/TheDiscoveryCard.vue';
-</script>
-
-<template>
-    <RouterView/>
-</template>
-
-
+import PaintCanvas from './Kraal/components/testing/PaintCanvas.vue'
+import Page from './Kraal/components/testing/Page.vue'
+import Needle from './Kraal/components/Needle.vue'
+import Thread from './Kraal/components/Thread.vue'
+import Base from './Kraal/components/Base.vue'
+import TheSwan from './components/custom/TheSwan/TheSwan.vue'
+import TheDiscovery from './components/TheDiscovery/TheDiscovery.vue';
+import SpindleWhorl from './components/custom/SpindleWhorl.vue';
+import BattleOfTorquay from "./components/BattleOfTorquay/BattleOfTorquay.vue";
+import PangolinAndCrocodile from "./components/PangolinAndCrocodile/PangolinAndCrocodile.vue";
 </script>
 
 <style scoped>
