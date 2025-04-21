089e1beefa8badf0796389a34c9abf41274aaec9
Alec Watson <alecwatson15@gmail.com>
2025-04-21 22:49:43 +0200

    Revert "Merge branch 'demo' into feature/the-kraal"
    This reverts commit bccd24b5a99eec16757666e556d55b32a892dcbc, reversing
    changes made to 613e67ea1398e7ea31e32b27198d0b3a7a066efc.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,22 @@
-<script setup lang="ts">
+<template>
+  <Base />
+</template>
+
+<script setup>
 import PaintCanvas from './Kraal/components/testing/PaintCanvas.vue'
 import Page from './Kraal/components/testing/Page.vue'
 import Needle from './Kraal/components/Needle.vue'
 import Thread from './Kraal/components/Thread.vue'
 import Base from './Kraal/components/Base.vue'
-import HelloWorld from './components/custom/HelloWorld.vue'
-import TheDiscovery from './components/TheDiscovery/TheDiscovery.vue';
-import SpindleWhorl from './components/custom/SpindleWhorl.vue';
-import BattleOfTorquay from "./components/BattleOfTorquay/BattleOfTorquay.vue";
-import PangolinAndCrocodile from "./components/PangolinAndCrocodile/PangolinAndCrocodile.vue";
+
+
+
 </script>
 
-<style scoped>
-
-
+<style>
+body {
+  margin: 0;
+  font-family: system-ui, sans-serif;
+  background-color: #f9fafb;
+}
 </style>
