82101c6a882ef970eaf0c102140a5bb2a3971fd4
Alec Watson <111232411+Alec-Watson@users.noreply.github.com>
2025-04-21 22:35:59 +0200

    Revert "Feature/the kraal"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-import PaintCanvas from './Kraal/components/testing/PaintCanvas.vue'
-import Page from './Kraal/components/testing/Page.vue'
-import Needle from './Kraal/components/Needle.vue'
-import Thread from './Kraal/components/Thread.vue'
-import Base from './Kraal/components/Base.vue'
 import HelloWorld from './components/custom/HelloWorld.vue'
 import TheDiscovery from './components/TheDiscovery/TheDiscovery.vue';
 import SpindleWhorl from './components/custom/SpindleWhorl.vue';
@@ -12,6 +7,5 @@ import PangolinAndCrocodile from "./components/PangolinAndCrocodile/PangolinAndC
 </script>
 
 <style scoped>
-
 
 </style>
