0020079f119ab164beefb3f0be20a9c48f843d19
Bigmanfish1 <igbojinnavictor@gmail.com>
2025-06-20 19:22:59 +0200

    Add security group configuration for Curio application

--- iac/main.tf
+++ iac/main.tf
@@ -2,10 +2,72 @@ provider "aws" {
   region = "eu-west-1" 
 }
 
+resource "aws_security_group" "curio_security_group" {
+  name        = "curio-app-sg"
+  description = "Security group for Curio application"
+
+  # Custom TCP - Port 5000
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this for production
+    description = "Custom TCP"
+  }
+
+  # HTTP - Port 80
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  # HTTPS - Port 443
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+  # SSH - Port 22
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this to your IP for security
+    description = "SSH"
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "curio-security-group"
+  }
+}
+
+# Output the security group ID for reference
+output "security_group_id" {
+  value = aws_security_group.curio_security_group.id
+}
+
 resource "aws_instance" "this-instance" {
   ami                         = "ami-0d5688bba1210af52"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.curio_security_group.id
+  ]
   user_data                   = <<-EOT
    
      #!/bin/bash
